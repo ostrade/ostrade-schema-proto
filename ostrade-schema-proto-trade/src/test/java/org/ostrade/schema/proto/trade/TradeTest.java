53f3025260dad7035fce7c456dc2fe7848a9cadd
snimmo <stephennimmo@gmail.com>
2023-07-23 08:37:31 -0500

    Started trade structures, pivoting to instrument definitions.

---
+++ ostrade-schema-proto-trade/src/test/java/org/ostrade/schema/proto/trade/TradeTest.java
@@ -0,0 +1,13 @@
+package org.ostrade.schema.proto.trade;
+
+import org.junit.jupiter.api.Test;
+
+public class TradeTest {
+
+    @Test
+    void trade() {
+        TradeV1.Trade trade = TradeV1.Trade.newBuilder()
+                .build();
+    }
+
+}
